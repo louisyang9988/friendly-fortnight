2bf523641044003a03d23566df64f866f455c8b7
louisyang9988 <louisyang9988@gmail.com>
2016-11-01 15:46:59 -0400

    Add files via upload

---
+++ test/tt.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Yang on 2016/9/9.
+ */
+var arr=[1,1,2,3,4,5]
+var unique = arr.filter(function(elem, index, self) {
+   // return index == self.indexOf(elem);
+    
+});
+
+console.log(unique);
